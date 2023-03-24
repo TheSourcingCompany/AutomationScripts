b5cf4faaedb0007c67ea1390a5f9a93820a7fbff
Anthony <anthony@beckman-lapre.nl>
2023-03-24 10:56:32 +0100

    Create StartWindowsService.ps1

---
+++ StartWindowsService.ps1
@@ -0,0 +1,19 @@
+# Start the service
+Start-Service $serviceName
+
+# Wait for the service to start
+$timeout = New-TimeSpan -Minutes 10
+$sw = [Diagnostics.Stopwatch]::StartNew()
+
+while ((Get-Service $serviceName).Status -ne "Running") {
+    # Check if the timeout has been exceeded
+    if ($sw.Elapsed -ge $timeout) {
+        Write-Error "Error: The $serviceName service failed to start within 10 minutes."
+        exit 1
+    }
+
+    # Wait for 1 second before checking the status again
+    Start-Sleep -Seconds 1
+}
+
+Write-Host "The $serviceName service has started successfully."
